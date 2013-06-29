610bd3db6657499b9ee6fc4be2ee794006860058
Michael Onikienko <michael.onikienko@gmail.com>
2013-06-29 20:16:19 +0300

    added missing `js\storage.js`  file

---
+++ js/storage.js
@@ -0,0 +1,7 @@
+var storage = {
+    // `sync` if you want. remember about QUOTAS https://developer.chrome.com/extensions/area.html#properties
+    area: chrome.storage.local,
+    default_options: {
+        // one key for every option
+    }
+};
