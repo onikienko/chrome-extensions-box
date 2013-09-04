394c9d87fbd8c541fe1fe705a222e87c54fc90b8
Michael Onikienko <michael.onikienko@gmail.com>
2013-09-04 16:05:18 +0300

    Update quick_options.js

--- js/helpers/quick_options.js
+++ js/helpers/quick_options.js
@@ -1,9 +1,6 @@
 /**
  * This is a part of Chrome Extensions Box
  * Read more on GitHub - https://github.com/onikienko/chrome-extensions-box
- *
- * Parse html and replace all {{property name from message.json}} from text nodes, title, alt, value and placeholder attrs
- * with chrome.i18n.getMessage http://developer.chrome.com/extensions/i18n.html
  */
 window.addEventListener('load', function () {
 
